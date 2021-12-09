91938795d2e940adf12e5ee645997a146c371f4c
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-09 10:00:23 +0100

    Add project files.

---
+++ AdventOfCode_Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿int lastVal = 0;
+int incrementCount = 0;
+
+using (var reader = new StreamReader("Input.txt"))
+{
+    lastVal = int.Parse(reader.ReadLine());
+    while (!reader.EndOfStream)
+    {
+        int newVal = int.Parse(reader.ReadLine());
+
+        if (newVal > lastVal) incrementCount++;
+        lastVal = newVal;
+    }    
+}
+
+Console.WriteLine(incrementCount);
