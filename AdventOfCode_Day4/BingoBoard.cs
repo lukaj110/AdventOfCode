4c6cef56459b960894a9260c5d51e918dd9b409f
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-15 21:13:35 +0100

    day 4 finished

---
+++ AdventOfCode_Day4/BingoBoard.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+
+namespace AdventOfCode_Day4
+{
+    public class BingoBoard
+    {
+        public short[,] Board = new short[5, 5];
+        List<short> ChosenValues = new List<short>();
+
+        public BingoBoard(string[] Input)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                short[] row = Input[i].Split(' ').Where(e => e.Length > 0).Select(e => short.Parse(e)).ToArray();
+                for (int j = 0; j < 5; j++) Board[i, j] = row[j];
+            }
+        }
+
+        public void ChooseNumber(short Value) => ChosenValues.Add(Value);
+
+        public bool Won
+        {
+            get
+            {
+                List<short> horizontal = new List<short>();
+                List<short> vertical = new List<short>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        horizontal.Add(Board[i, j]);
+                        vertical.Add(Board[j, i]);
+                    }
+                    if (horizontal.All(e => ChosenValues.Contains(e)) || vertical.All(e => ChosenValues.Contains(e))) return true;
+
+                    horizontal.Clear();
+                    vertical.Clear();
+                }
+
+                return false;
+            }
+
+        }
+
+        public short SumUnmarked
+        {
+            get
+            {
+                List<short> values = new List<short>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        short val = Board[i, j];
+
+                        if (!ChosenValues.Contains(val)) values.Add(val);
+                    }
+
+                }
+
+                return (short)values.Sum(e=>e);
+            }
+        }
+
+        public int FinalScore => SumUnmarked * ChosenValues.Last();
+    }
+}
---
+++ AdventOfCode_Day4/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode_Day4;
+
+List<short> chosenNumbers = new List<short>();
+List<BingoBoard> bingoBoards = new List<BingoBoard>();
+
+using (var reader = new StreamReader("Input.txt"))
+{
+    foreach (string chosenNumber in reader.ReadLine().Split(",")) chosenNumbers.Add(short.Parse(chosenNumber));
+
+    string[] inputs = reader.ReadToEnd().Split(Environment.NewLine).Where(e => e.Length > 0).ToArray();
+
+    for (int i = 0; i < inputs.Length; i += 5) bingoBoards.Add(new BingoBoard(inputs[i..(i + 5)]));
+}
+
+//Part 1
+
+List<BingoBoard> wonBoards = new List<BingoBoard>();
+
+foreach (short chosenNumber in chosenNumbers)
+{
+    foreach (BingoBoard bingoBoard in bingoBoards)
+    {
+        if (!bingoBoard.Won) bingoBoard.ChooseNumber(chosenNumber);
+        else if (bingoBoard.Won && !wonBoards.Contains(bingoBoard)) wonBoards.Add(bingoBoard);
+    }
+}
+
+Console.WriteLine(wonBoards.First().FinalScore);
+
+//Part 2
+
+Console.WriteLine(wonBoards.Last().FinalScore);
