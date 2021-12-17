e521aba4f25301560af7ef1875952d57cba234d1
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-17 12:34:19 +0100

    finished day 5

--- AdventOfCode_Day5/Line.cs
+++ AdventOfCode_Day5/Line.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace AdventOfCode_Day5
     {
         public Vector2 From { get; set; }
         public Vector2 To { get; set; }
+        public float Slope => (To.Y - From.Y) / (To.X - From.X);
 
         public Line(Point Point1, Point Point2)
         {
@@ -19,9 +21,53 @@ namespace AdventOfCode_Day5
             To = new Vector2(Point2.X, Point2.Y);
         }
 
-        public IEnumerable<Point> GetPointsBetween()
+        public IEnumerable<Point> GetPointsBetweenHorizontalVertical()
         {
-            
+            if (From.X == To.X)
+            {
+                if (From.Y > To.Y) for (int i = (int)To.Y; i <= From.Y; i++) yield return new Point((int)From.X, i);
+                else for (int i = (int)From.Y; i <= To.Y; i++) yield return new Point((int)From.X, i);
+            }
+            else if (From.Y == To.Y)
+            {
+                if (From.X > To.X) for (int i = (int)To.X; i <= From.X; i++) yield return new Point(i, (int)From.Y);
+                else for (int i = (int)From.X; i <= To.X; i++) yield return new Point(i, (int)From.Y);
+            }
+        }
+
+        public IEnumerable<Point> GetPointsBetweenDiagonal()
+        {
+            if (Math.Abs(Slope) == 1)
+            {
+                if (From.X < To.X && From.Y < To.Y)
+                {
+                    for (int i = 0; i <= To.X - From.X; i++)
+                    {
+                        yield return new Point((int)From.X + i, (int)From.Y + i);
+                    }
+                }
+                else if (From.X > To.X && From.Y > To.Y)
+                {
+                    for (int i = 0; i <= From.X - To.X; i++)
+                    {
+                        yield return new Point((int)From.X - i, (int)From.Y - i);
+                    }
+                }
+                if (From.X > To.X && From.Y < To.Y)
+                {
+                    for (int i = 0; i <= From.X - To.X; i++)
+                    {
+                        yield return new Point((int)From.X - i, (int)From.Y + i);
+                    }
+                }
+                else if (From.X < To.X && From.Y > To.Y)
+                {
+                    for (int i = 0; i <= To.X - From.X; i++)
+                    {
+                        yield return new Point((int)From.X + i, (int)From.Y - i);
+                    }
+                }
+            }
         }
     }
 }
--- AdventOfCode_Day5/Program.cs
+++ AdventOfCode_Day5/Program.cs
@@ -1,3 +1,47 @@
 ﻿using AdventOfCode_Day5;
+using System.Drawing;
+using System.Numerics;
 
 List<Line> lines = new List<Line>();
+
+using (var reader = new StreamReader("Input.txt"))
+{
+    while (!reader.EndOfStream)
+    {
+        string[] points = reader.ReadLine().Split(" -> ");
+        string[] point1 = points[0].Split(",");
+        string[] point2 = points[1].Split(",");
+
+        lines.Add(new Line(new Point(int.Parse(point1[0]), int.Parse(point1[1])), new Point(int.Parse(point2[0]), int.Parse(point2[1]))));
+    }
+}
+
+//Part 1
+
+int[,] Board = new int[1000, 1000];
+
+foreach (Line line in lines)
+{
+    foreach (Point point in line.GetPointsBetweenHorizontalVertical())
+    {
+        Board[point.X, point.Y] = Board[point.X, point.Y] + 1;
+    }
+}
+
+int result = Board.Cast<int>().Where(e => e >= 2).Count();
+
+Console.WriteLine(result);
+
+//Part 2
+
+foreach (Line line in lines)
+{
+    foreach (Point point in line.GetPointsBetweenDiagonal())
+    {
+        Board[point.X, point.Y] = Board[point.X, point.Y] + 1;
+    }
+}
+
+result = Board.Cast<int>().Where(e => e >= 2).Count();
+
+Console.WriteLine(result);
