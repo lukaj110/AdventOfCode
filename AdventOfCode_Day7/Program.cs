36516d057ebecced0292362f9768049b4c514778
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-17 14:46:10 +0100

    day 7 part 1 brute forced :/

---
+++ AdventOfCode_Day7/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> submarines = new List<int>();
+
+using (var reader = new StreamReader("Input.txt"))
+{
+    foreach (var submarine in reader.ReadLine().Split(","))
+    {
+        submarines.Add(int.Parse(submarine));
+}
+}
+submarines.Sort();
+
+int closest = submarines.Aggregate((x, y) => Math.Abs(x - submarines.Average()) < Math.Abs(y - submarines.Average()) ? x : y);
+
+List<int> positions = new List<int>();
+
+for(int x = 0; x < 2000; x++)
+{
+    int totalFuel = 0;
+
+    for (int i = 0; i < submarines.Count; i++)
+    {
+        totalFuel += x > submarines[i] ? x - submarines[i] : submarines[i] - x;
+    }
+
+    positions.Add(totalFuel);
+
+    Console.WriteLine($"Position {x}: {totalFuel}");
+}
+
+Console.WriteLine(positions.Min());
