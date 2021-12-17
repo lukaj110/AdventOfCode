a1dcb00cf8dac6b4733cb08a4b26bb4a2cd791e5
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-17 09:26:46 +0100

    started day 5

---
+++ AdventOfCode_Day5/Line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_Day5
+{
+    public class Line
+    {
+        public Vector2 From { get; set; }
+        public Vector2 To { get; set; }
+
+        public Line(Point Point1, Point Point2)
+        {
+            From = new Vector2(Point1.X, Point1.Y);
+            To = new Vector2(Point2.X, Point2.Y);
+        }
+
+        public IEnumerable<Point> GetPointsBetween()
+        {
+            
+        }
+    }
+}
---
+++ AdventOfCode_Day5/Program.cs
@@ -0,0 +1,3 @@
+﻿using AdventOfCode_Day5;
+
+List<Line> lines = new List<Line>();
