52265091538790ce6263f7e8c60117603a56d140
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-19 13:04:48 +0100

    day 11 finished

---
+++ AdventOfCode_Day11/OctopusBoard.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_Day11
+{
+    public class Octopus
+    {
+        public bool Flashed { get; set; } = false;
+        public int Value { get; set; }
+
+        public Octopus(int Value)
+        {
+            this.Value = Value;
+        }
+    }
+    public class OctopusBoard
+    {
+        public Octopus[,] Board = new Octopus[10, 10];
+
+        public int FlashCounter = 0;
+
+        public void AddValue(int Row, int Col, int Value) => Board[Row, Col] = new Octopus(Value);
+
+        public void IncreaseAll(int Value)
+        {
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++) Board[i, j].Value += Value;
+            }
+        }
+
+        public IEnumerable<Tuple<int, int>> GetAdjacentOctopuses(int Row, int Col)
+        {
+            if (Row > 0 && Row < Board.GetLength(0) - 1)
+            {
+                if(Col > 0 && Col < Board.GetLength(1) - 1)
+                {
+                    yield return Tuple.Create(Row - 1, Col - 1);
+                    yield return Tuple.Create(Row - 1, Col + 1);
+                    yield return Tuple.Create(Row + 1, Col + 1);
+                    yield return Tuple.Create(Row + 1, Col - 1);
+                }else if (Col == 0)
+                {
+                    yield return Tuple.Create(Row - 1, Col + 1);
+                    yield return Tuple.Create(Row + 1, Col + 1);
+                }
+                else
+                {
+                    yield return Tuple.Create(Row - 1, Col - 1);
+                    yield return Tuple.Create(Row + 1, Col - 1);
+                }
+
+                yield return Tuple.Create(Row - 1, Col);
+                yield return Tuple.Create(Row + 1, Col);
+            }
+            else if (Row == 0)
+            {
+                if (Col > 0 && Col < Board.GetLength(1) - 1)
+                {
+                    yield return Tuple.Create(Row + 1, Col - 1);
+                    yield return Tuple.Create(Row + 1, Col + 1);
+                }
+                else if (Col == 0) yield return Tuple.Create(Row + 1, Col + 1);
+                else yield return Tuple.Create(Row + 1, Col - 1);
+                yield return Tuple.Create(Row + 1, Col);
+            }
+            else
+            {
+                if (Col > 0 && Col < Board.GetLength(1) - 1)
+                {
+                    yield return Tuple.Create(Row - 1, Col - 1);
+                    yield return Tuple.Create(Row - 1, Col + 1);
+                }
+                else if (Col == 0) yield return Tuple.Create(Row - 1, Col + 1);
+                else yield return Tuple.Create(Row - 1, Col - 1);
+                yield return Tuple.Create(Row - 1, Col);
+            }
+
+            if (Col > 0 && Col < Board.GetLength(1) - 1)
+            {
+                yield return Tuple.Create(Row, Col - 1);
+                yield return Tuple.Create(Row, Col + 1);
+            }
+            else if (Col == 0) yield return Tuple.Create(Row, Col + 1);
+            else yield return Tuple.Create(Row, Col - 1);
+        }
+
+        public IEnumerable<Tuple<int, int>> GetAdjacentOctopusesCanFlash(int Row, int Col)
+        {
+            return GetAdjacentOctopuses(Row, Col).Where(e => !Board[e.Item1, e.Item2].Flashed);
+        }
+
+        public void Flash(int Row, int Col)
+        {
+            FlashCounter += 1;
+            Board[Row, Col].Flashed = true;
+            Board[Row, Col].Value = 0;
+
+            foreach(var octopus in GetAdjacentOctopuses(Row, Col))
+            {
+                if(!Board[octopus.Item1, octopus.Item2].Flashed)
+                {
+                    Board[octopus.Item1, octopus.Item2].Value += 1;
+                    if(Board[octopus.Item1, octopus.Item2].Value > 9)
+                    {
+                        Flash(octopus.Item1, octopus.Item2);
+                    }
+                }
+            }
+        }
+
+        public void ClearFlashes()
+        {
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++) Board[i, j].Flashed = false;
+            }
+        }
+    }
+}
---
+++ AdventOfCode_Day11/Program.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCode_Day11;
+
+List<string> input = new List<string>();
+
+using (var reader = new StreamReader("Input.txt"))
+{
+    while (!reader.EndOfStream)
+    {
+        input.Add(reader.ReadLine());
+    }
+}
+
+//Part 1
+
+OctopusBoard octopusBoard = new OctopusBoard();
+
+for (int i = 0; i < input.Count; i++)
+{
+    for (int j = 0; j < input[i].Length; j++)
+    {
+        octopusBoard.AddValue(i, j, int.Parse(input[i][j].ToString()));
+    }
+}
+
+for (int i = 1; i <= 100; i++)
+{
+    octopusBoard.IncreaseAll(1);
+
+    for (int j = 0; j < octopusBoard.Board.GetLength(0); j++)
+    {
+        for (int k = 0; k < octopusBoard.Board.GetLength(1); k++)
+        {
+            if (octopusBoard.Board[j, k].Value > 9 && !octopusBoard.Board[j, k].Flashed)
+            {
+                octopusBoard.Flash(j, k);
+            }
+        }
+    }
+
+    octopusBoard.ClearFlashes();
+}
+
+Console.WriteLine(octopusBoard.FlashCounter);
+
+//Part 1
+
+OctopusBoard octopusBoard2 = new OctopusBoard();
+
+for (int i = 0; i < input.Count; i++)
+{
+    for (int j = 0; j < input[i].Length; j++)
+    {
+        octopusBoard2.AddValue(i, j, int.Parse(input[i][j].ToString()));
+    }
+}
+
+int step = 1;
+bool allFlashed = false;
+
+while (!allFlashed)
+{
+    octopusBoard2.IncreaseAll(1);
+
+    for (int i = 0; i < octopusBoard2.Board.GetLength(0); i++)
+    {
+        for (int j = 0; j < octopusBoard2.Board.GetLength(1); j++)
+        {
+            if (octopusBoard2.Board[i, j].Value > 9 && !octopusBoard2.Board[i, j].Flashed)
+            {
+                octopusBoard2.Flash(i, j);
+            }
+        }
+    }
+
+    if (octopusBoard2.Board.Cast<Octopus>().All(e => e.Flashed)) allFlashed = true;
+    else
+    {
+        octopusBoard2.ClearFlashes();
+        step++;
+    }
+}
+
+Console.WriteLine(step);
