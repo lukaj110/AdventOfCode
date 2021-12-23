4ced7a09a920e059bcd538f3ec7a04402eae0231
lukaj110 <jovanovic.l01@htlwienwest.at>
2021-12-23 09:27:12 +0100

    day 12 part 1

---
+++ AdventOfCode_Day12/Cave.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_Day12
+{
+    public class Cave
+    {
+        public string Name { get; set; }
+        public List<Cave> ConnectedTo { get; set; } = new List<Cave>();
+        public bool BigCave { get; set; }
+
+        public IEnumerable<IEnumerable<Cave>> TraverseToEndPart1(IEnumerable<Cave> traversedPath)
+        {
+            traversedPath = traversedPath.Append(this);
+
+            if (Name == "end")
+            {
+                yield return traversedPath;
+                yield break;
+            }
+
+            foreach (Cave cave in ConnectedTo)
+            {
+                if ((!cave.BigCave && traversedPath.Any(e => e.Name == cave.Name)) || cave.Name == "start") continue;
+
+                foreach (var traversedToEnd in cave.TraverseToEndPart1(traversedPath.ToList())) yield return traversedToEnd;
+            }
+        }
+    }
+}
---
+++ AdventOfCode_Day12/Program.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode_Day12;
+
+List<string> input = new List<string>();
+
+using (var reader = new StreamReader("Input.txt"))
+{
+    while (!reader.EndOfStream)
+    {
+        input.Add(reader.ReadLine());
+    }
+}
+
+//Part 1
+
+List<Cave> caves = new List<Cave>();
+
+foreach (string line in input.SelectMany(e => e.Split("-")).Distinct())
+{
+    caves.Add(new Cave()
+    {
+        BigCave = Char.IsUpper(line[0]),
+        Name = line
+    }); ;
+}
+
+foreach (string[] line in input.Select(e => e.Split("-")))
+{
+    caves.Where(e => e.Name == line[0]).Single().ConnectedTo.Add(caves.Where(e => e.Name == line[1]).Single());
+
+    caves.Where(e => e.Name == line[1]).Single().ConnectedTo.Add(caves.Where(e => e.Name == line[0]).Single());
+}
+
+Cave startNode = caves.Where(e => e.Name == "start").Single();
+
+var paths = startNode.TraverseToEndPart1(new List<Cave>());
+
+Console.WriteLine(paths.Count());
+
+//Part 2
